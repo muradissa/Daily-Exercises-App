3032741ece8227c6d3fe5a33f4d72bc04935f713
Murad EIssa <muradissa00@gmail.com>
2023-10-01 20:55:18 +0300

    first commit

---
+++ lib/widgets/bottom_nav_bar.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:daily_exercises_app/constants.dart';
+
+class BottomNavBar extends StatelessWidget {
+  const BottomNavBar({
+     Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 40, vertical: 10),
+      height: 80,
+      color: Colors.white,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          BottomNavItem(
+            title: "Today",
+            svgScr: "assets/icons/calendar.svg",
+            press: () =>{},
+          ),
+          BottomNavItem(
+            title: "All Exercises",
+            svgScr: "assets/icons/gym.svg",
+            press: () =>{},
+            isActive: true,
+          ),
+          BottomNavItem(
+            title: "Settings",
+            svgScr: "assets/icons/Settings.svg",
+            press: () =>{},
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class BottomNavItem extends StatelessWidget {
+  final String svgScr;
+  final String title;
+  final Function press;
+  final bool isActive;
+  const BottomNavItem({
+     Key? key,
+    required this.svgScr,
+    required this.title,
+    required this.press,
+    this.isActive = false,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: press(),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: <Widget>[
+          SvgPicture.asset(
+            svgScr,
+            color: isActive ? kActiveIconColor : kTextColor,
+          ),
+          Text(
+            title,
+            style: TextStyle(color: isActive ? kActiveIconColor : kTextColor),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class SearchBar extends StatelessWidget {
+  const SearchBar({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 30),
+      padding: EdgeInsets.symmetric(horizontal: 30, vertical: 5),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(29.5),
+      ),
+      child: TextField(
+        decoration: InputDecoration(
+          hintText: "Search",
+          icon: SvgPicture.asset("assets/icons/search.svg"),
+          border: InputBorder.none,
+        ),
+      ),
+    );
+  }
+}
